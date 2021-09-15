be13ead4ac6517556ba6465b3f7920fa8e52869e
Half-A-Turnip <1456907177@qq.com>
2021-09-15 23:34:52 +0800

    unplugin-element-plus 插件上传

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import ElementPlus from './unplugin-element-plus/dist/vite'
+import ElementPlus from './unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
